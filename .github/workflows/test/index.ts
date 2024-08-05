ce059525576920f510c314f2ffbf94e2c07eea75
drunkwinter <38593134+drunkwinter@users.noreply.github.com>
2024-08-05 17:35:03 +0200

    fix

--- .github/workflows/test/index.ts
+++ .github/workflows/test/index.ts
@@ -1,12 +1,14 @@
 import * as core from '@actions/core';
 
-async function run() {
-    try {
-        throw 'Your mom is too hot';
-    } catch (error) {
-        // Fail the workflow run if an error occurs
-        if (error instanceof Error) core.setFailed(error.message)
-    }
-}
+core.setFailed('YURMOM!!!!!')
 
-run();
+// async function run() {
+//     try {
+//         throw 'Your mom is too hot';
+//     } catch (error) {
+//         // Fail the workflow run if an error occurs
+//         if (error instanceof Error) core.setFailed(error.message)
+//     }
+// }
+
+// run();
