763e4394ec260e6e7f400ee8c3aaa2604a022590
drunkwinter <38593134+drunkwinter@users.noreply.github.com>
2024-08-05 17:05:50 +0200

    fix

--- .github/workflows/test/index.ts
+++ .github/workflows/test/index.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 
 async function run() {
     try {
-        core.debug('Hello there!!!')
+        core.info('Hello there!!!')
     } catch (error) {
         // Fail the workflow run if an error occurs
         if (error instanceof Error) core.setFailed(error.message)
